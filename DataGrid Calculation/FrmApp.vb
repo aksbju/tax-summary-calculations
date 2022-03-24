ea26cfaaa72f71f0751a841c2069896348281f94
Akash Shaw <aksbju@gmail.com>
2022-03-24 20:19:19 +0530

    Update

---
+++ DataGrid Calculation/FrmApp.vb
@@ -0,0 +1,15 @@
+﻿Public Class FrmApp
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim frmTax As FrmTax = New FrmTax()
+        Me.Visible = False
+        frmTax.ShowDialog()
+        Me.Close()
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim frmGst As FrmGst = New FrmGst()
+        Me.Visible = False
+        frmGst.ShowDialog()
+        Me.Close()
+    End Sub
+End Class
--- DataGrid Calculation/My Project/Application.Designer.vb
+++ DataGrid Calculation/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.DataGrid_Calculation.FrmGst
+            Me.MainForm = Global.DataGrid_Calculation.FrmApp
         End Sub
     End Class
 End Namespace
